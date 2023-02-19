7c44fc36c67320b61cd8cd62b1fd4fa42d4b6f32
Broc0De <123789970+Broc0De@users.noreply.github.com>
2023-02-19 14:42:58 +0000

    Update main.js
    lab3

--- main.js
+++ main.js
@@ -3,29 +3,26 @@ const iphones = [
 	{
 		make: 'Apple',
 		model: 'Iphone 14 Pro Max',
-		year: 2023,
-		price: 3000,
+		
+		price: 300000,
 		image: 'https://cdn.vodafone.co.uk/en/assets/images/desktop/Apple_iPhone_14_Pro_Max_deep_purple-full-product-front-600.png'
 	},
 	{
 		make: 'Apple',
 		model: 'Iphone 13 Pro',
-		year: 2022,
-		price: 2500,
+		price: 250000,
 		image: 'https://ismart.co.in/wp-content/uploads/2021/09/iphone-13-pro-blue-select.png'
 	},
 	{
 		make: 'Apple',
 		model: 'iphone 12',
-		year: 2022,
-		price: 1700,
+		price: 270000,
 		image: 'https://res-4.cloudinary.com/grover/image/upload/v1630928555/oip6emk98qshpfcismyh.png'
 	},
 	{
 		make: 'Apple',
 		model: 'Iphone 11 Pro Max',
-		year: 2020,
-		price: 2000,
+		price: 200000,
 		image: 'https://cdn1.it4profit.com/brd2/q:68/g:sm/plain/s3://app/public/models/MWHJ2FS-A/large/j/220117170017062726.jpg'
 	},
 	
@@ -41,8 +38,7 @@ function displayCars(_iphones) {
 		iphoneEl.innerHTML = `
 			<img src="${Iphone.image}" alt="${Iphone.make} ${Iphone.model}">
 			<h3>${Iphone.make} ${Iphone.model}</h3>
-			<p>Year: ${Iphone.year}</p>
-			<p>Price: $${Iphone.price}</p>
+			<p>Цена: ${Iphone.price} тенге</p>
 		`;
 		iphoneList.appendChild(iphoneEl);
 	});
